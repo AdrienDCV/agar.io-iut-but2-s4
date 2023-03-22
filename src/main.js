1c6e4cf9e4857a24b3dc8d570e056e7664af37e4
AdrienDCV <adrien.dacostaveiga@gmail.com>
2023-03-22 16:26:54 +0100

    premiers tests déplacements joueur souris

--- src/main.js
+++ src/main.js
@@ -4,18 +4,27 @@ import Player from "./Player.js";
 const canvas = document.querySelector('.gameCanvas');
 const context = canvas.getContext('2d');
 
-let x = 0;
-let y = 0;
-let xDirection = 0;
-let yDirection = 0;	
-
 const player1 = new Player(100,100,50, new Color(`#FF0000`), context, 'Parppaing', 0);
 const player2 = new Player(200,200,50, new Color(`#AE1D23`), context, 'Parppaing', 0);
 player1.drawDot();
 player2.drawDot();
 
 window.onkeydown = move;
-//window.onkeyup = stopMove;
+
+canvas.addEventListener("mousemove", mouseMoveHandler, false);
+
+function mouseMoveHandler(e) {
+	const relativeX = e.clientX - canvas.offsetLeft;
+	const relativeY = e.clientY - canvas.offsetLeft;
+	if (relativeX > 0 && relativeX < canvas.clientWidth) {
+	  player1.xPosition = relativeX - player1.radius / 2;
+	}
+	if (relativeY > 0 && relativeY < canvas.clientHeight) {
+		player1.yPosition = relativeY - player1.radius / 2;
+	}
+
+	render();
+}
 
 function render() {
 	context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
@@ -24,12 +33,10 @@ function render() {
 	requestAnimationFrame(render);
 }
 
-window.onkeydown = move;
-
 function move(event) {
-	console.log(`${player1.xPosition}`);
-	console.log(`+ ${player1.xPosition < canvas.clientWidth}`);
-	console.log(`- ${player1.xPosition > canvas.clientWidth}`);
+	// console.log(`${player1.xPosition}`);
+	// console.log(`+ ${player1.xPosition < canvas.clientWidth}`);
+	// console.log(`- ${player1.xPosition > canvas.clientWidth}`);
 	
 	if (player1.xPosition <= 0 || player1.xPosition <= canvas.clientWidth) {
 		if (event.code == 'ArrowRight') {
